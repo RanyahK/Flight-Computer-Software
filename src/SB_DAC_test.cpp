caa5deadbb4141a4f7d89289b95e56598f1f99e8
unknown <ranyahzk@udel.edu>
2025-01-31 14:31:41 -0500

    SB_DAC software updated, plot with Arduino IDE, reads ADCs for Vsweep, Vsweep2, and Probe 1 Differential and Gain Stage

--- src/SB_DAC_test.cpp
+++ src/SB_DAC_test.cpp
@@ -12,16 +12,59 @@ void writeTCA9539(uint8_t reg, uint8_t value);
 #define CONFIG_PORT1 0x07
 #define OUTPUT_PORT0 0x02
 #define OUTPUT_PORT1 0x03
+#define LAST_ARDUINO_PIN_NUMBER LEDB + 1
+#define RESET_PIN LAST_ARDUINO_PIN_NUMBER + PE_3
 
 const int WRITE = 0b0001000000000000;   
 const int WRITEB = 0b1001000000000000;   
 
 const int mask = 0x0FFF;  
 
+int vsweep = A1;
+int vsweep2 = A2;
+int diff = A6;
+int gain = A3;
+int arr[256];
+
+void print(int i){
+
+  arr[i] = analogRead(vsweep);
+    float vsweep_out = (analogRead(vsweep)* (3.3 / 65535.0) * 8) - 12;
+    analogRead(A0);
+
+    float vsweep2_out = (analogRead(vsweep2) * (3.3 / 65535.0) * 8) -12;
+    analogRead(A0);
+
+    float diff_out = (analogRead(diff) * (3.3 /  65535.0) * 8) -12;
+    analogRead(A0);
+
+   float gain_out = (analogRead(gain)*(3.3 /  65535.0) * 8) - 12;
+   analogRead(A0);
+  
+   //Serial.println(val/16);
+  // Serial.println(voltage);
+ if((i%8)==0){
+  Serial.print(vsweep_out);      //the first variable for plotting
+    Serial.print(",");              //seperator
+    Serial.print(vsweep2_out);          //the second variable for plotting including line break
+    Serial.print(",");              //seperator
+    Serial.print(diff_out);          //the second variable for plotting including line break
+    Serial.print(",");              //seperator
+    Serial.println(gain_out);          //the second variable for plotting including line break
+ }
+
+}
 
 void setup() {
   Wire.begin();  // Initialize I2C
   Serial.begin(9600);
+  analogReadResolution(16);
+  pinMode(RESET_PIN, OUTPUT);
+  digitalWrite(RESET_PIN, LOW);
+  delay(10);  // Hold reset low for 10ms
+  digitalWrite(RESET_PIN, HIGH);
+  delay(10);  // Wait for the device to initialize
+
   
   // Writing 0x00 to CONFIG_PORT0 and CONFIG_PORT1 makes all pins outputs
   writeTCA9539(CONFIG_PORT0, 0x00);  // Set all Port 0 pins as outputs
@@ -29,8 +72,8 @@ void setup() {
 
   // Set specific pins high
   //Set the first two pins and fourth pin of Port 0 high (0b00001011).
-  writeTCA9539(OUTPUT_PORT0, 0b11111111);  // Set P0_0 and P0_1 high on Port 0
-  writeTCA9539(OUTPUT_PORT1, 0b11111111);  // Set all pins low on Port 1
+  writeTCA9539(OUTPUT_PORT0, 0b00010000);  // Set P0_0 and P0_1 high on Port 0
+  writeTCA9539(OUTPUT_PORT1, 0b00000000);  // Set all pins low on Port 1
 
   Serial.println("GPIO pins set high on TCA9539");
 
@@ -65,6 +108,7 @@ void loop() {
       int x = i * 16;
       writeRegister(x);
       delay(10);
+      print(i);
     }
 }
 
